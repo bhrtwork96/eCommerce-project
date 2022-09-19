eb8efdcd03e458383a8f725349cdffbae165a78d
bhrtwork96 <bhrtmali96@gmail.com>
2022-09-19 10:47:53 +0530

    commit on 19-Sep-22

---
+++ migrations/20220828083330-create-books.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Books', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.TEXT
+      },
+      author: {
+        type: Sequelize.TEXT
+      },
+      PublishOn: {
+        type: Sequelize.DATE
+      },
+      AddedOn: {
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Books');
+  }
+};
